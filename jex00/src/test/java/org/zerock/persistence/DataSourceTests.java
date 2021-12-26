03bd9a541618dfc499d6a9ef15c6ef6ad5ca0916
dnjsgur0629 <sisun_@naver.com>
2021-12-27 00:23:33 +0900

    part1-ch4 MyBatis and Spring Integration

--- jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration("filefile:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class DataSourceTests {
 
@@ -31,10 +31,9 @@ public class DataSourceTests {
 	
 	@Test
 	public void testMyBatis() {
-		
 		try(SqlSession session = sqlSessionFactory.openSession();
 				Connection con = session.getConnection();
-			){
+				){
 			log.info(session);
 			log.info(con);
 		} catch (Exception e) {
