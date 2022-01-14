94d9dc766b2e2f489f81aae555ee010850629784
dnjsgur0629 <sisun_@naver.com>
2022-01-15 04:31:35 +0900

    part5 ch20 Handling of the number of replies

--- ex02/src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -2,13 +2,17 @@ package org.zerock.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyPageDTO;
 import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.BoardMapper;
 import org.zerock.mapper.ReplyMapper;
 
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
@@ -16,12 +20,19 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class ReplyServiceImpl implements ReplyService{
 	
+	@Setter (onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
+	@Setter (onMethod_ = @Autowired)
+	private BoardMapper boardMapper;
+	
+	@Transactional
 	@Override
 	public int register(ReplyVO vo) {
 		// TODO Auto-generated method stub
 		log.info("register........." + vo);
+		
+		boardMapper.updateReplyCnt(vo.getBno(), 1);
 		
 		return mapper.insert(vo);
 	}
@@ -42,10 +53,14 @@ public class ReplyServiceImpl implements ReplyService{
 		return mapper.update(vo);
 	}
 
+	@Transactional
 	@Override
 	public int remove(Long rno) {
 		// TODO Auto-generated method stub
 		log.info("remove........." + rno);
+		
+		ReplyVO vo = mapper.read(rno);
+		boardMapper.updateReplyCnt(vo.getBno(), -1);
 		
 		return mapper.delete(rno);
 	}
